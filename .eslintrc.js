8318225c6f712c8cd2dc1f97f8099b30fc0e7394
Gurvan Henry <gurvan.henry@gmail.com>
2022-08-12 09:47:28 +0200

    Update eslint stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,7 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'plugin:jest/recommended'],
   plugins: ['detox', 'jest'],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+  },
 };
